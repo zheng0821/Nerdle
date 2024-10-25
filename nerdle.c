1bc1ea89ae01540d4d2871cfd0a9224661ec2353
RC_Hu <2410155737@qq.com>
2024-10-25 22:48:01 +0800

    2.0.0

--- nerdle.c
+++ nerdle.c
@@ -13,7 +13,8 @@ void print_rule();
 
 //函数组1：输入检测
 int is_valid_equation(char input[]);//检查输入是否合法
-int cal(int a,int b,char op);//计算算式
+float op(int n[],char s[],int tn,int ts);
+float cal(float a,float b,char op);//计算算式
 
 //函数组2：输入匹配
 int check_diff(char input[]);//比较初始与输入算式
@@ -23,20 +24,23 @@ int en_num(char a);//将符号转为地址
 void random_equation();//随机生成算式
 char random_sign(int seed);//随机生成计算符
 int random_num(int digit,int seed);//随机生成digit位数字
-int quick_check();//快速遴选  提高效率
+int quick_check(int loc,char stick[],int tail);//快速遴选  提高效率
 int digit_num(int n);//判断位数
 
 int main(){
-    print_rule();
     random_equation();
+
+    print_rule();
+
     int n=1,key=0;
+
     while(n<=MAX_GUESS_FRE){
         char guess[20];
-        printf("---- GUESS #%d ----\n     ",n++);
-        scanf("%s",guess);printf("  ");
+        printf("------------------ GUESS #%d ------------------\n                   ",n++);
+        scanf("%s",guess);
         if (is_valid_equation(guess)) {
             if (check_diff(guess)) {
-                printf("---- SUCCESS! ----");
+                printf("------------------ SUCCESS! ------------------");
                 key=1;
                 break;
             }
@@ -45,7 +49,8 @@ int main(){
         }
     }
     if(!key){
-        printf("----- LOSE! -----\n");
+        printf("-------------------- LOSS --------------------\n");
+        printf("               ANS:");
         for(int i=0;i<8;i++){
             printf("%s",st_equation[i]);
         }
@@ -55,13 +60,23 @@ int main(){
 }
 
 void print_rule(){
-    printf("------------------NERDLE_RULE------------------\n");
+    printf("------------------NERDLE_RULE-----------------\n");
     printf(" ! is not in the target equation at all\n");
     printf(" ^ is in the equation ,but in the wrong space\n");
     printf(" $ is in the equation and in the correct spot\n");
-    printf("-----------------------------------------------\n");
+    printf("----------------------------------------------\n");
     printf(" !  There is no more 0 before each word  !\n");
-    printf("-----------------------------------------------\n\n");
+    printf("----------------------------------------------\n\n");
+    printf("Developer model?(Y/n)");
+    char input;
+    scanf("%c",&input);
+    if(input=='Y'){
+        printf("ANS:");
+        for(int i=0;i<8;i++){
+            printf("%c",st_equation[i]);
+        }
+    }
+    printf("\n\n");
 }
 
 
@@ -75,23 +90,23 @@ int is_valid_equation(char input[]){
         return 0;
     }//ckpt(checkpoint)1：长度
 
-    int top_num=-1,top_sign=-1,stick_num[8];char stick_sign[8];
+    int tail_num=-1,tail_sign=-1,stick_num[8];char stick_sign[8];
     int temp=0;
     int i=0,flag=0;//遍历
     while(i<8){
         if (input[i]=='+'||input[i]=='-'||input[i]=='*'||input[i]=='/'){
             if(flag){
-                stick_num[++top_num]=temp;
+                stick_num[++tail_num]=temp;
                 temp=0;
                 flag=0;
             }
-            stick_sign[++top_sign]=input[i];//后缀栈
+            stick_sign[++tail_sign]=input[i];
         }else if(input[i]=='='){
             if(temp==0){
                 printf("! Invalid equation\n");
                 return 0;
             }else{
-                stick_num[++top_num]=temp;
+                stick_num[++tail_num]=temp;
                 break;
             }
         }else if('0'<=input[i]&&input[i]<='9'){
@@ -118,31 +133,23 @@ int is_valid_equation(char input[]){
         }//ckpt4：等式右侧
     }
 
-    if(top_sign==-1){
+    if(tail_sign==-1){
         printf("! There must be at least one sign + - * /.\n");
         return 0;
     }
-    if(top_num!=1+top_sign){
+    if(tail_num!=1+tail_sign){
         printf("! Invalid equation.\n");
         return 0;
     }//ckpt5：符号个数及匹配
 
-    while(top_num!=0){
-        if(stick_sign[top_sign]=='/'){//ckpt6：运算要求
-            if(stick_num[top_num]==0){
-                printf("! Invalid equation.\n");
-                return 0;
-            }
-            float temp_div=stick_num[top_num-1]*1.0/stick_num[top_num];
-            if(floor(temp_div)!=ceil(temp_div)){
-                printf("! Invalid equation.\n");
-                return 0;
-            }//出现小数一定不可能相等
+    for(int i=0;i<=tail_sign;i++){
+        if(stick_sign[i]=='/'&&stick_num[i+1]==0){//ckpt6：运算要求
+            printf("! Invalid equation.\n");
+            return 0;
         }
-        stick_num[top_num-1]=cal(stick_num[top_num],stick_num[top_num-1],stick_sign[top_sign]);
-        top_num--;top_sign--;//
     }
-    if(stick_num[top_num]!=RHS){
+    float  LHS=op(stick_num,stick_sign,tail_num,tail_sign);
+    if(LHS!=(float)RHS){
         printf("! The left side is not equal to the right.\n");
         return 0;
     }//ckpt7：等号成立
@@ -150,12 +157,12 @@ int is_valid_equation(char input[]){
     return 1;
 }
 
-int cal(int a,int b,char op){
+float cal(float a,float b,char op){
     switch(op){
         case '+':return b+a;
         case '-':return b-a;
         case '*':return b*a;
-        case '/':return b/a;
+        case '/':return b*1.0/a;
     }
 }
 
@@ -171,7 +178,7 @@ int check_diff(char input[]){
             count[en_num(st_equation[i])]+=1;
         }
     }
-    printf("   ");
+    printf("                   ");
     if(sgn==8){
         printf("$$$$$$$$\n");
         return 1;
@@ -212,22 +219,22 @@ void random_equation(){
 
         int i=-1;
         int end=0;//记录最后一个字符位置
-        char stick_sign[2]="oo";int top_sign=-1;
-        int stick_num[3]={0,0,0};int top_num=-1;
+        char stick_sign[2]="oo";int tail_sign=-1;
+        int stick_num[3]={0,0,0};int tail_num=-1;
 
         while(i<loc_eq-2){//再确定符号位置
+            seed=(seed)%9+1;
             srand((unsigned int)time(NULL)*seed);
             i+=rand()%3+2;
 
             if(i<loc_eq-1){
                 st_equation[i]=random_sign(seed);
-
-                stick_sign[++top_sign]=st_equation[i];
+                stick_sign[++tail_sign]=st_equation[i];
                 end=i;
             }
         }
 
-        if(quick_check(loc_eq,stick_sign,top_sign)){
+        if(quick_check(loc_eq,stick_sign,tail_sign)){
             continue;
         }//情况特判，提高效率
 
@@ -236,62 +243,48 @@ void random_equation(){
         int j=0,beg=0;
         while(1){//前面几位随机生成，最后一位遍历
             if(j==end){
-                stick_num[++top_num]=random_num(j-beg,seed);
+                stick_num[++tail_num]=random_num(j-beg,(seed)%9+1);
                 break;
             }else if(st_equation[j]=='+'||st_equation[j]=='-'||
                       st_equation[j]=='*'||st_equation[j]=='/'){
-                stick_num[++top_num]=random_num(j-beg,seed);
+                stick_num[++tail_num]=random_num(j-beg,(seed)%9+1);
                 beg=i+1;
             }
             j++;
         }
 
         int num_min=(int)pow(10.0,(float)(loc_eq-end-2));
-        int stick_ans[100],top_ans=-1;
+        if(num_min==0){num_min++;}
+        int stick_ans[100],tail_ans=-1;
         for(int i=num_min;i<num_min*10;i++){
-            int temp=i,temp_tn=top_num,temp_ts=top_sign;
-            while(temp_tn!=-1){
-                if(stick_sign[temp_ts]=='/'){
-                    if(temp==0){temp=-404;break;}
-                    float temp_div=stick_num[temp_tn]*1.0/temp;
-                    if(floor(temp_div)!=ceil(temp_div)){temp=-404;break;}
-                }//除法特别考虑
-                temp=cal(temp,stick_num[temp_tn],stick_sign[temp_ts]);
-                temp_tn--;temp_ts--;
-            }
-            if(temp>0&&digit_num(temp)==7-loc_eq){//匹配位数
+            stick_num[tail_num+1]=i;
+            float temp=op(stick_num,stick_sign,tail_num,tail_sign);
+            if(temp>=0&&digit_num((int)temp)==7-loc_eq){//匹配位数
                 flag=1;
-                stick_ans[++top_ans]=i;//记录全部可行值再随机抽取
-                if(top_ans==99){break;}
+                stick_ans[++tail_ans]=i;//记录全部可行值再随机抽取
+                if(tail_ans==99){break;}
             }
         }
 
         if(flag){
             srand((unsigned int)time(NULL)*seed);
             int random_ans;
-            if(top_ans!=0){random_ans=rand()%(top_ans);}
+            if(tail_ans!=0){random_ans=rand()%(tail_ans);}
             else{random_ans=0;}
-            stick_num[++top_num]=stick_ans[random_ans];//随机选取最后的运算数
+            stick_num[++tail_num]=stick_ans[random_ans];//随机选取最后的运算数
 
-            int RHS=stick_ans[random_ans];
-            int temp_top_num=top_num-1;
-            while(temp_top_num!=-1){
-                RHS=cal(RHS,stick_num[temp_top_num],stick_sign[top_sign]);
-                temp_top_num--;top_sign--;
-            }//计算右值
-
+            int RHS=op(stick_num,stick_sign,tail_num,tail_sign);
             for(int i=7;i>loc_eq;i--){
                 st_equation[i]=RHS%10+'0';
                 RHS/=10;
             }
             for(int i=loc_eq-1;i>=0;i--){
                 if(st_equation[i]=='0'){
-                    st_equation[i]=stick_num[top_num]%10+'0';
-                    stick_num[top_num]/=10;
-                    if(stick_num[top_num]==0){top_num--;}
+                    st_equation[i]=stick_num[tail_num]%10+'0';
+                    stick_num[tail_num]/=10;
+                    if(stick_num[tail_num]==0){tail_num--;}
                 }
             }//更新字符数组
-
         }
     }
 }
@@ -317,11 +310,11 @@ int random_num(int digit,int seed){
     }
 }
 
-int quick_check(int loc,char stick[],int top){
-    if(top==-1){return 1;}
-    if(top==0){
-        if(loc==6&&(stick[top]=='+'||stick[top]=='*')){return 1;}
-        if(loc==4&&(stick[top]=='-'||stick[top]=='/')){return 1;}
+int quick_check(int loc,char stick[],int tail){
+    if(tail==-1){return 1;}
+    if(tail==0){
+        if(loc==6&&(stick[tail]=='+'||stick[tail]=='*')){return 1;}
+        if(loc==4&&(stick[tail]=='-'||stick[tail]=='/')){return 1;}
     }
     return 0;//有可能生成
 }
@@ -334,4 +327,16 @@ int digit_num(int n){
         digit++;
     }
     return digit;
+}
+
+float op(int n[],char s[],int tn,int ts){
+    if(ts==0){
+        return cal(n[1],n[0],s[0]);
+    }else{
+        if(s[1]=='*'||s[1]=='/'){
+            return cal(cal(n[2],n[1],s[1]),n[0],s[0]);
+        }else{
+            return cal(n[2],cal(n[1],n[0],s[0]),s[1]);
+        }
+    }
 }
